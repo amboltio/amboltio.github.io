7a8a00b604b2f388f35fa215ec087f2861dfce9e
Mikkel Alexander Madsen <mikkelarm@gmail.com>
2016-07-21 10:30:51 +0200

    removed shitty files, add slider files, new css components

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,3 +137,16 @@
 	});
 
 })(jQuery);
+
+
+
+
+$(document).ready(function(){
+  $('.slider').slick({
+	dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
